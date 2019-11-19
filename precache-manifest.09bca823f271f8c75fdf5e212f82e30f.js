6665487229a8c2503957acbe21f6f86e7564f18a
byseop <byseop@gmail.com>
2019-11-19 14:08:55 +0900

    Updates

---
+++ precache-manifest.09bca823f271f8c75fdf5e212f82e30f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f61707a4f83f1dd312453f3d9f366c",
+    "url": "/modern-movie-web/index.html"
+  },
+  {
+    "revision": "983b17f2c6717f6309e7",
+    "url": "/modern-movie-web/static/css/main.0adcc361.chunk.css"
+  },
+  {
+    "revision": "b48b247ecc24e5311238",
+    "url": "/modern-movie-web/static/js/2.12c93f71.chunk.js"
+  },
+  {
+    "revision": "983b17f2c6717f6309e7",
+    "url": "/modern-movie-web/static/js/main.f028df77.chunk.js"
+  },
+  {
+    "revision": "b183f604ba23e3c64e75",
+    "url": "/modern-movie-web/static/js/runtime-main.c1a18d46.js"
+  }
+]);
